2e58de4d005ca8f746767e8cc1b6c15a434a1712
cestweka <darmawanweka1@gmail.com>
2025-04-08 06:09:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {   
+    return {
+        name: "Member",
+        description: "Member of the team",
+        skills: [
+            {
+                name: "Teamwork",
+                description: "Ability to work well with others"
+            },
+            {
+                name: "Communication",
+                description: "Ability to communicate effectively"
+            }
+        ]
+    };
+}
